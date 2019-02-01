77ade1ad19a0fabb7ce89b0f629c7b2377d668e2
cha0sCat <cmt7221@gmail.com>
2019-02-01 15:12:16 +0800

    调整flask_wechat位置

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,7 @@
-#微信消息处理框架导入
-from flask_wechatpy import Wechat, wechat_required, oauth
-
 #从plugin目录调用所有注册了的插件
 from flask import Flask
 flask_app = Flask(__name__, instance_relative_config=True)
 flask_app.config.from_object('config') #根目录下面的config
 flask_app.config.from_pyfile('config.py') #instance下面的config
+#加载flask路由配置
 from .import router
--- app/router.py
+++ app/router.py
@@ -1,6 +1,8 @@
 from . import flask_app
 from flask import request
 from .models.response import wechat_response
+#从plugin目录调用所有注册了的插件
+from .flask_wechatpy import Wechat, wechat_required, oauth
 #路由文件
 #访问不同网址由本文件处理
 
