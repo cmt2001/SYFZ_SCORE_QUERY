25f254e4ffd5b1aca5d33a9b7aaac95f9b5fa9e4
cha0sCat <cmt7221@gmail.com>
2019-02-05 02:23:33 +0800

    精简

--- main/plugins/articles.py
+++ main/plugins/articles.py
@@ -1,50 +1,6 @@
 # -*- coding: utf-8 -*-
 from . import register_keyword
-from wechatpy.replies import ArticlesReply
 from wechatpy.replies import create_reply
 
-@register_keyword(u'图文')
-def reply_articles(msg):
-    articles = [
-        {
-            'title': 'this a test',
-            'description': 'this is a description',
-            'url': 'localhost',
-        },
-        {
-        'title': 'this a test2',
-            'description': 'this is a description2',
-            'url': 'localhost2',
-        },
-    ]
+def reply_articles(msg,articles):
     return create_reply(articles,msg)
-
-'''
-def reply_articles(msg):
-    return make_articles(
-        msg, {
-            'title': 'this a test',
-            'description': 'this is a description',
-            'url': 'localhost',
-        })
-'''
-
-def make_articles(msg, articles):
-    '''
-    向用户返回图文消息
-    :msg 经过解析的用户dict
-    :articles 文章list
-    '''
-    reply = ArticlesReply(message=msg)
-
-    if isinstance(articles, list):
-        for article in articles:
-            reply.add_article(article)
-    elif isinstance(articles, dict):
-        reply.add_article(articles)
-    else:
-        from .. import app
-        app.logger.warning(
-            'make_articles cant handle this message!\n%s' % articles)
-
-    return reply
